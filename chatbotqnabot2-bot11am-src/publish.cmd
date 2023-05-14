6380b7ad817cce77d87db63efcc1e49d7bf9fb87
leeh356 <leeh356@users.noreply.github.com>
2023-05-14 16:49:43 +0800

    Add files via upload

---
+++ chatbotqnabot2-bot11am-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABotWithMSI.sln -p:DeployOnBuild=true -p:PublishProfile=.\chatbotqnabot2-bot-ff95-Web-Deploy.pubxml -p:Password=MYKKz8fzX0EECAJGPN4jn4rNgdteXkZ8diakn6apgEqmBofwJkTJGj8BAvr8
+
